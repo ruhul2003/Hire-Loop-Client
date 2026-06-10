14f0b46f59c01d8dcd4c24d13ed422d5e7e3e34a
ruhul2003 <ruhul941020@gmail.com>
2026-06-11 02:37:40 +0600

    Added new job form

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecruiterJobs = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4">My Job Postings</h1>
+        </div>
+    );
+};
+
+export default RecruiterJobs;
